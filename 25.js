029cad6d66f6fb62a1c6566e918b0c28c8b54785
“Moggach” <acunnane13@gmail.com>
2021-07-10 20:18:14 +0100

    adding my own example of code reading challenge

---
+++ 25.js
@@ -0,0 +1,45 @@
+function f (arg1, arg2) {
+    if (arg1.length <= arg2.length) {
+      return [...arg1, ...arg2]
+    }
+   
+    else return []
+  }
+  
+  
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+ 
+ 
+  
+console.log(f([14, 2], [1]));
+console.log(f([14, 2], [1]));
+console.log(f([14, 2], [1]));
+console.log(f([14, 2], [1]));
